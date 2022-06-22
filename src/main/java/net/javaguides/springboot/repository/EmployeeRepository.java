da1e419546983bdb9352b8662cb998f3ea97a5e8
ObulaReddy Thavva <obulareddyjd@gmail.com>
2022-06-22 23:12:20 +0530

    add method and changed database name

--- src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -1,11 +1,14 @@
 package net.javaguides.springboot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import net.javaguides.springboot.model.Employee;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+	@Query(value = "SELECT * FROM employees WHERE last_name = ?1", nativeQuery = true)
+	Employee getEmployeeDetails(String lastName);
 }
